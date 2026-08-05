ffcc6f3d7efd5844361f5eb3952d0566b1c41da3
Yatharth <yatharths675@gmail.com>
2026-08-05 20:22:15 +0530

    New file added
    Added a Python UI based calculator, optimised to handle errors (mostly)

---
+++ Calculator.py
@@ -0,0 +1,47 @@
+is_running = True
+
+print("Welcome to Calculator")
+print("Enter 1 for addition")
+print("Enter 2 for subtraction")
+print("Enter 3 for mutiplication")
+print("Enter 4 for division")
+
+while is_running:
+    choice = int(input("What operation would you like to do?: "))
+
+    if choice == 1:
+        try:
+            x = int(input("Enter first number: "))
+            y = int(input("Enter second number: "))
+            print("Answer is",x+y)
+        except:
+            print("Error")
+    elif choice == 2:
+        try:
+            x = int(input("Enter first number: "))
+            y = int(input("Enter second number: "))
+            print("Answer is",x-y)
+        except:
+            print("Error")
+    elif choice == 3:
+        try:
+            x = int(input("Enter first number: "))
+            y = int(input("Enter second number: "))
+            print("Answer is",x*y)
+        except:
+            print("Error")
+    elif choice == 4:
+        try:
+            x = int(input("Enter first number: "))
+            y = int(input("Enter second number: "))
+            print("Answer is",x/y)
+        except:
+            print("Error")
+    else:
+        print("Invaild choice")
+
+    rerun = input("Would you like to rerun the program? (Q to quit): ")
+    if rerun.lower() == "q":
+        is_running = False
+
+
