59d540751f93c78e1c31566db546244958f1165e
Yatharth <yatharths675@gmail.com>
2026-08-13 20:25:47 +0530

    Improve input handling in Calculator
    Added error handling for invalid operation input.

--- Calculator.py
+++ Calculator.py
@@ -7,8 +7,12 @@ print("Enter 3 for mutiplication")
 print("Enter 4 for division")
 
 while is_running:
-    choice = int(input("What operation would you like to do?: "))
-
+    choice = None
+    try:
+        choice = int(input("What operation would you like to do?: "))
+    except ValueError:
+        pass
+        
     if choice == 1:
         try:
             x = int(input("Enter first number: "))
@@ -43,5 +47,6 @@ while is_running:
     rerun = input("Would you like to rerun the program? (Q to quit): ")
     if rerun.lower() == "q":
         is_running = False
+
 
 
