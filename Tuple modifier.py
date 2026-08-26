b9ae87f79024d9c9c55d979ba496a993f38f2ef3
Yatharth <yatharths675@gmail.com>
2026-08-26 20:54:44 +0530

    Delete Tuple modifier.py

--- Tuple modifier.py
+++
@@ -1,8 +0,0 @@
-#tuple is immutable meaning it cant be changed so it must be converted to a list for changes
-t=(12,13,16,15)
-t=list(t) #changes to list
-t[2]=14
-t=tuple(t) #changes to tuple
-print(t)
-
-
