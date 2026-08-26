79be6f5519b2b4fcc215a96d91ded6456b3c7e5b
Yatharth <yatharths675@gmail.com>
2026-08-26 20:43:56 +0530

    Delete First n prime numbers.py

--- First n prime numbers.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the value of n: "))
-for num in range(2, n + 1, 1):
-    track = True
-    for i in range(2, num):
-        if num % i == 0:
-            track = False
-            break
-    if track:
-        print(num)
